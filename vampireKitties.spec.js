b7c24ddb680ec0ad294959a089552e0a14ce9522
Nan Davenport <nannigan@nannigans.com>
2015-07-02 18:44:59 -0700

    changed path in karma config preprocessors

--- vampireKitties.spec.js
+++ vampireKitties.spec.js
@@ -1,21 +1,21 @@
 'use strict';
-describe('vampireKittiesLogIn', function() {
-    //following documentation https://docs.angularjs.org/guide/unit-testing
-    var $compile,
+    describe('vampireKittiesLogIn', function() {
+        //following documentation https://docs.angularjs.org/guide/unit-testing
+        var $compile,
         $rootsScope;
 
 
-beforeEach(module('vampireKitties')); //module with directive being tested on
-beforeEach(module('vampire-kitties.html'));//template partial
+    beforeEach(module('vampireKitties')); //module with directive being tested on
+    beforeEach(module('vampire-kitties.html'));//template partial
 
 
-beforeEach(inject(function (_$compile_, _$rootScope_) {
-  $compile = _$compile_;
-  $rootsScope = _$rootScope_;
+    beforeEach(inject(function (_$compile_, _$rootScope_) {
+      $compile = _$compile_;
+      $rootsScope = _$rootScope_;
 
-}));
+    }));
 
-it('should render the elements on log in html page', function(){
+    it('should render the elements on log in html page', function(){
 
         var element = $compile("<opt-in></opt-in>")($rootScope);
         $rootScope.$digest();
